88b0c84c0afac4369c40c977f64dcd7619588a5c
Bhagyashri-Patil10 <patilbhagyashri.patil@gmail.com>
2020-04-13 15:12:45 +0530

    hi new code

---
+++ demo/src/demo/Hi.java
@@ -0,0 +1,14 @@
+package demo;
+
+
+public class Hi {
+	static{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		
+	System.out.println("hello");
+	}
+
+}
